1eed788d52c25425e3b66da05cab0758db83acf2
hasindusithmin <hasindusithmin64@gmail.com>
2022-04-26 11:09:06 +0530

    add route(path variable)

--- server.go
+++ server.go
@@ -1,12 +1,21 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
+	})
+
+	app.Get("/hello/:name", func(c *fiber.Ctx) error {
+		msg := fmt.Sprintf("Hello, %s", c.Params("name"))
+		return c.SendString(msg)
 	})
 
 	app.Listen(":3000")
